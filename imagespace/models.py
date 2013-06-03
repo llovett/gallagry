ed9eb1260c0e99cc5212c89b3fd46e21217d89e7
Luke Lovett <llovett@oberlin.edu>
2013-06-03 13:27:30 -0600

    changed field names for imageframe

--- imagespace/models.py
+++ imagespace/models.py
@@ -4,11 +4,12 @@ from sorl.thumbnail import ImageField
 class ImageFrame(models.Model):
     image = ImageField(upload_to='upload')
     
-    pos_x = models.IntegerField()
-    pos_y = models.IntegerField()
-    width = models.IntegerField()
-    height = models.IntegerField()
-    price = models.DecimalField(max_digits=8,decimal_places=2,default=0)
+    pos_x = models.IntegerField(default=0, verbose_name="X Position")
+    pos_y = models.IntegerField(default=0, verbose_name="Y Position")
+    width = models.IntegerField(default=100, verbose_name="Width")
+    height = models.IntegerField(default=100, verbose_name="Height")
+    price = models.DecimalField(max_digits=8,decimal_places=2,default=0, verbose_name="Price")
+    visible = models.BooleanField(default=True, verbose_name="Visible")
 
-    title = models.CharField(max_length=500)
-    description = models.TextField(blank=True,null=True)
+    title = models.CharField(max_length=500, verbose_name="Title")
+    description = models.TextField(blank=True,null=True, verbose_name="Description")
