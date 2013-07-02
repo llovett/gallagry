a0ed219e032933c76f8ae0ef9d3016c66bd84a82
Luke Lovett <lukeharmonlovett@gmail.com>
2013-07-01 23:39:51 -0400

    some cleanup of barebones views, some styling of barebones templates

--- barebones/views.py
+++ barebones/views.py
@@ -4,20 +4,29 @@ from barebones.models import Entry
 from settings import models as prefs
 from tagging.models import Tag, TaggedItem
 from re import sub
+from functools import wraps
 
-def main_page(request):
+def get_prefs(view):
+    @wraps(view)
+    def wrapper(request, *args, **kwargs):
+        try:
+            colorscheme = prefs.ColorScheme.objects.get(use_for_blog=True)
+        except prefs.ColorScheme.DoesNotExist:
+            colorscheme = None
+        try:
+            bgimage = prefs.BackgroundImage.objects.get(use_for_blog=True)
+        except prefs.BackgroundImage.DoesNotExist:
+            bgimage = None
+        return view(request, colorscheme, bgimage, *args, **kwargs)
+    return wrapper
+    
+@get_prefs
+def main_page(request, colorscheme, bgimage):
     entries = Entry.objects.all()
-    try:
-        colorscheme = prefs.ColorScheme.objects.get(use_for_blog=True)
-    except prefs.ColorScheme.DoesNotExist:
-        pass
-    try:
-        bgimage = prefs.BackgroundImage.objects.get(use_for_blog=True)
-    except prefs.BackgroundImage.DoesNotExist:
-        pass
     return render_to_response("entry_list.html", locals(), context_instance=RequestContext(request))
 
-def get_tagged(request, slugified_tag):
+@get_prefs
+def get_tagged(request, colorscheme, bgimage, slugified_tag):
     # Have to look through all the tags, because of multi-word tag/slugify issue
     all_tags = Tag.objects.usage_for_model(Entry)
     tag = None
@@ -27,24 +36,9 @@ def get_tagged(request, slugified_tag):
             break
     tag = Tag.objects.get(name=tag)
     entries = TaggedItem.objects.get_by_model(Entry, tag)
-    try:
-        colorscheme = prefs.ColorScheme.objects.get(use_for_blog=True)
-    except prefs.ColorScheme.DoesNotExist:
-        pass
-    try:
-        bgimage = prefs.BackgroundImage.objects.get(use_for_blog=True)
-    except prefs.BackgroundImage.DoesNotExist:
-        pass
     return render_to_response("entry_list.html", locals(), context_instance=RequestContext(request))
 
-def get_post(request, slugified_title):
+@get_prefs
+def get_post(request, colorscheme, bgimage, slugified_title):
     entry = get_object_or_404(Entry, slug=slugified_title)
-    try:
-        colorscheme = prefs.ColorScheme.objects.get(use_for_blog=True)
-    except prefs.ColorScheme.DoesNotExist:
-        pass
-    try:
-        bgimage = prefs.BackgroundImage.objects.get(use_for_blog=True)
-    except prefs.BackgroundImage.DoesNotExist:
-        pass
     return render_to_response("single_entry.html", locals(), context_instance=RequestContext(request))
