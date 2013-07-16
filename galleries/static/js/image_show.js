efea63d8a62b3b46b609edb171122bae5d83df99
Luke Lovett <lukeharmonlovett@gmail.com>
2013-07-16 23:32:03 +0000

    fixed some width issues

--- galleries/static/js/image_show.js
+++ galleries/static/js/image_show.js
@@ -2,6 +2,7 @@ $(document).ready(
     function() {
 	// Calculate width of body
 	var total_width = 0;
+	var win_width = $(window).width();
 	var images = $(".gallery_image");
 	if (images.length > 0) {
 	    images.each(
@@ -12,8 +13,9 @@ $(document).ready(
 	    );
 	    // 10px padding on wrapper, gallery on each side, plus some as buffer
 	    total_width += 50;
+
 	    console.log(total_width);
-	    $("body").css('width',total_width+"px");
+	    $("body").css('width',(total_width < win_width ? win_width : total_width)+"px");
 	}
     }
 );
