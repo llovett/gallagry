6c716e123aac2b8a042e866cb7b276052e9c46b7
Luke Lovett <lukeharmonlovett@gmail.com>
2013-07-26 17:17:24 +0000

    link position default uniqueness enforced at model level

--- frontpage/models.py
+++ frontpage/models.py
@@ -6,6 +6,13 @@ class GalleryLinkPosition(models.Model):
     rotation = models.DecimalField(max_digits=8,decimal_places=5, default=0, verbose_name="Rotation")
     is_default = models.BooleanField(default=True, verbose_name="Make this the default gallery link position")
 
+    def save(self):
+        if self.is_default:
+            for other in GalleryLinkPosition.objects.exclude(id=self.id):
+                other.is_default = False
+                other.save()
+        super(GalleryLinkPosition, self).save()
+
     def __unicode__(self):
         return "gallery at (%d,%d) rotated %f"%(self.pos_x,self.pos_y,self.rotation)
 
@@ -14,6 +21,13 @@ class BlogLinkPosition(models.Model):
     pos_y = models.IntegerField(default=0, verbose_name="Y Position")
     rotation = models.DecimalField(max_digits=8,decimal_places=5, default=0, verbose_name="Rotation")
     is_default = models.BooleanField(default=True, verbose_name="Make this the default blog link position")
+
+    def save(self):
+        if self.is_default:
+            for other in BlogLinkPosition.objects.exclude(id=self.id):
+                other.is_default = False
+                other.save()
+        super(BlogLinkPosition, self).save()
 
     def __unicode__(self):
         return "blog at (%d,%d) rotated %f"%(self.pos_x,self.pos_y,self.rotation)
