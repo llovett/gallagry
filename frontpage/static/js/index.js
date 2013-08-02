4d2a008ea36f32ef30232c56b501b271ed8faf15
Luke Lovett <lukeharmonlovett@gmail.com>
2013-08-02 10:07:40 -0700

    changed marquee library. think this works better

--- frontpage/static/js/index.js
+++ frontpage/static/js/index.js
@@ -1,7 +1,25 @@
 $(document).ready(function(){
-    $('#gallery-marquee').simplemarquee({
-	speed: 		5000,
-	direction: 	'rtl', // Options "ltr" or "rtl"
-	pause: 		false
-    });
+    // Marquee scrolling solution from http://stackoverflow.com/questions/2143056/seamless-jquery-marquee
+    var marquee = $("#gallery-marquee");
+
+    marquee.css({"overflow": "hidden", "width": "420px", "height": "200px"});
+
+    // wrap "My Text" with a span (old versions of IE don't like divs inline-block)
+    marquee.wrapInner("<span>");
+    marquee.find("span").css({ "width": "50%", "display": "inline-block", "text-align":"center" }); 
+    marquee.append(marquee.find("span").clone()); // now there are two spans with "My Text"
+
+    // create an inner div twice as wide as the view port for animating the scroll
+    marquee.wrapInner("<div>");
+    marquee.find("div").css("width", "200%");
+
+    // create a function which animates the div
+    // $.animate takes a callback for when the animation completes
+    var reset = function() {
+        $(this).css("margin-left", "0%");
+        $(this).animate({ "margin-left": "-100%" }, 3000, 'linear', reset);
+    };
+
+    // kick it off
+    reset.call(marquee.find("div"));
 });
