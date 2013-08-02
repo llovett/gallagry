6aa02564cc0dde529eccd1ff4ca28b64584727db
Luke Lovett <lukeharmonlovett@gmail.com>
2013-08-02 07:26:51 -0700

    added batch image editing capability inside of galleries admin

--- galleries/admin.py
+++ galleries/admin.py
@@ -2,12 +2,17 @@ from django.contrib import admin
 from galleries.models import Image, Gallery
 from sorl.thumbnail.admin import AdminImageMixin
 
+class ImageInlineAdmin(admin.TabularInline, AdminImageMixin):
+    model = Image
+
 class ImageAdmin(admin.ModelAdmin, AdminImageMixin):
     list_display = ('title','price', 'description')
     search_fields = ('title','price')
-
+    
 class GalleryAdmin(admin.ModelAdmin):
-    pass
+    inlines = [
+        ImageInlineAdmin
+    ]
 
 admin.site.register(Image, ImageAdmin)
 admin.site.register(Gallery, GalleryAdmin)
